ddaf37190385c09085bc28499ccbed9f1b21a971
Arshad Iqubal <aiqubal2@gmail.com>
2026-02-16 18:54:31 +0530

    third message

--- Specs/stress-test.js
+++ Specs/stress-test.js
@@ -2,8 +2,8 @@ import http from 'k6/http';
 
 export const options = {
     stages: [
-        {duration: '1m', target: 100}, // Ramp up to 100 users over 1 minute
-        {duration: '3m', target: 100}, // Stay at 100 users for 3 minutes
+        {duration: '1m', target: 200}, // Ramp up to 100 users over 1 minute
+        {duration: '3m', target: 200}, // Stay at 100 users for 3 minutes
         {duration: '1m', target: 0}   // Ramp down to 0 users over 1 minute
     ]}
 
